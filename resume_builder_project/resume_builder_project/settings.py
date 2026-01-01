0a316a3c1270b82e8eaa82025ceb6396094f997a
Soham334 <soham.ks@somaiya.edu>
2026-01-02 00:12:51 +0530

    Fix ALLOWED_HOSTS for Render production

--- resume_builder_project/resume_builder_project/settings.py
+++ resume_builder_project/resume_builder_project/settings.py
@@ -24,7 +24,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-dev-key")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
